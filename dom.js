d70a00138918d3b3649921ff9b61a2dbed9248ee
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-02-20 04:07:56 +0300

    adding some scripts

--- dom.js
+++ dom.js
@@ -30,5 +30,23 @@ var jane = document.getElementById("jane").addEventListener("mouseover",rengiDeg
 function rengiDegistir(){
     document.getElementById("div1").style.color="red";
     var isimElemanlari=document.getElementsByName("CustomerName");
-    isimElemanlari[0].value="Hans"
-}
+//     isimElemanlari[0].value="Hans"
+// }
+
+var node = document.getElementById("tree");
+alert(node.childNodes[0].nodeValue);
+
+var baslik=document.createElement("h2");
+var node = document.createTextNode("Hello JS");
+baslik.appendChild(node)
+
+var div1= document.getElementById("div1");
+var p2 = document.getElementById("p2")
+div1.insertBefore(baslik,p2)
+
+alert("P2 is deteting now....")
+div1.removeChild(p2);
+
+alert("P1 is changing now....")
+var p1=document.getElementById("p1")
+div1.replaceChild(baslik,p1);
