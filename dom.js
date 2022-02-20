3a6f84691a5e821e58121786870629fe73ada2f8
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-02-20 03:40:22 +0300

    some changeds

--- dom.js
+++ dom.js
@@ -1,29 +1,29 @@
-// //document.getElementById("bio").innerHTML="John Doe: 2022"
-// var intro1 = document.getElementById("intro1");
+//document.getElementById("bio").innerHTML="John Doe: 2022"
+var intro1 = document.getElementById("intro1");
 
-// var mesaj = document.getElementById("mesaj");
+var mesaj = document.getElementById("mesaj");
 
-// mesaj.innerHTML = intro1.innerHTML;
+mesaj.innerHTML = intro1.innerHTML;
 
-// var tumlisteler = document.getElementsByTagName("ul");
-// var cities = tumlisteler[0];
+var tumlisteler = document.getElementsByTagName("ul");
+var cities = tumlisteler[0];
 
-// tumElemanlar = cities.getElementsByTagName("li");
+tumElemanlar = cities.getElementsByTagName("li");
 
-// for (i = 0; i < tumElemanlar.length; i++) {
-// //     alert(tumElemanlar[i].innerHTML);
-// // }
+for (i = 0; i < tumElemanlar.length; i++) {
+//     alert(tumElemanlar[i].innerHTML);
+// }
 
-//  var classElemanlari = document.getElementsByClassName("intro1")
-// alert(classElemanlari[1].innerHTML)
-// alert(classElemanlari.length)
+ var classElemanlari = document.getElementsByClassName("intro1")
+alert(classElemanlari[1].innerHTML)
+alert(classElemanlari.length)
 
 
-// // var queryElemanlari =document.querySelectorAll("p.intro1");
-// // alert(queryElemanlari.length);
+// var queryElemanlari =document.querySelectorAll("p.intro1");
+// alert(queryElemanlari.length);
 
-// var isimElemanlari = document.getElementsByName("CustomerName");
-// alert(isimElemanlari[0].value);
+var isimElemanlari = document.getElementsByName("CustomerName");
+alert(isimElemanlari[0].value);
 
 var jane = document.getElementById("jane").addEventListener("mouseover",rengiDegistir);
 
